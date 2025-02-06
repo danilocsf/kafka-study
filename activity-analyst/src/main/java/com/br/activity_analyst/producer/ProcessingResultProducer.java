f90a48926f8ee1e8726b7148de5e2b6221ac77a4
Danilo Ferreira <danilo.ferreira@matera.com>
2025-02-06 13:41:30 -0300

    Exemplo de envio de msg sincrono

--- activity-analyst/src/main/java/com/br/activity_analyst/producer/ProcessingResultProducer.java
+++ activity-analyst/src/main/java/com/br/activity_analyst/producer/ProcessingResultProducer.java
@@ -8,9 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class ProcessingResultProducer {
@@ -31,7 +33,20 @@ public class ProcessingResultProducer {
                 processing.processName(), String.valueOf(processing.id())));
         LOGGER.info("Sending information to topic " + processigResultTopic);
         String data = objectMapper.writeValueAsString(processing);
-        kafkaTemplate.send(processigResultTopic, null, data);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(processigResultTopic, null, data);
+
+        future.whenComplete((result, exception) -> {
+           if(exception != null){
+               LOGGER.error("Error information to topic " + processigResultTopic);
+           } else {
+               LOGGER.info("Information successfully sent to " + result.getRecordMetadata().topic());
+           }
+        });
+        LOGGER.info("Informação antes de finalizar");
+        /*Bloqueia a thread atual e espera pelo fim do processamento*/
+        future.join();
+        LOGGER.info("Informação depois que o processamento finalizou");
+
     }
 
 }
