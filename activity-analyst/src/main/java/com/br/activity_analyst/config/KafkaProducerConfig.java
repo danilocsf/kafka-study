eb163f4d29d7765e3b6fb7579b05ea3a9770d212
Danilo Ferreira <danilo.ferreira@matera.com>
2025-02-10 13:34:38 -0300

    Configuração do delivery. timeout. ms

--- activity-analyst/src/main/java/com/br/activity_analyst/config/KafkaProducerConfig.java
+++ activity-analyst/src/main/java/com/br/activity_analyst/config/KafkaProducerConfig.java
@@ -34,6 +34,12 @@ public class KafkaProducerConfig {
     @Value("${producer.retry.backoff.ms}")
     private int retryBackoffMs;
 
+    @Value("${producer.delivery.timeout")
+    private int deliveryTimeout;
+
+    @Value("${producer.ack}")
+    private String ack;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> properties = kafkaProperties.buildProducerProperties();
@@ -42,9 +48,17 @@ public class KafkaProducerConfig {
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
         properties.put(ProducerConfig.LINGER_MS_CONFIG, lingerMs);
+        properties.put(ProducerConfig.ACKS_CONFIG, ack);
         /*Pelo que entendi, esses retries são para casos em que o ACK não foi correspondido*/
-        properties.put(ProducerConfig.RETRIES_CONFIG, retry);
-        properties.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, retryBackoffMs);
+        //properties.put(ProducerConfig.RETRIES_CONFIG, retry);
+        //properties.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, retryBackoffMs);
+        /* Delivery timeout - Tempo máximo que um producer irá gastar tentando enviar uma mensagem.
+           Isso inclui o tempo de envio da solicitação, tempo em que espera pelo ACK de todas as in sync replicas
+           e o tempo em que gasta tentando novamente a operação.
+           Delivery timeout é aconselhado a ser utilizado ao invés de retry + retry.backoff.ms
+           Deve ter um valor maior que a soma de linger.ms e request.timeout.ms
+        */
+        properties.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         return new DefaultKafkaProducerFactory(properties);
     }
 
